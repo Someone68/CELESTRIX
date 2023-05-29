c1cd23c7181c2989e218bf42d918a2cf654e65ce
ducksquatarmy <unconfigured@null.spigotmc.org>
2023-05-29 15:59:49 -0700

    sdasd

--- game/game.js
+++ game/game.js
@@ -1,4 +1,26 @@
-let points = 0;
-function clicker() {
-	points = points + 1;
+let buttonElement = document.getElementById("btn");
+let stardustElement = document.getElementById("stardust");
+let stardust = 0;
+window.onload = () => {
+	buttonElement.addEventListener("click", click);
+	if (localStorage.stardust) {
+		updateDOM();
+	} else {
+		localStorage.stardust = 0;
+		updateDOM();
+	}
+	if (localStorage.clickPower) {
+		updateDOM;
+	} else {
+		localStorage.clickPower = 1;
+	}
+};
+
+function click() {
+	localStorage.stardust =
+		Number(localStorage.stardust) + Number(localStorage.clickPower);
+	updateDOM();
+}
+function updateDOM() {
+	stardustElement.innerHTML = "◈&#8201;" + localStorage.stardust;
 }
