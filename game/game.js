5d6f660d1d299ab8043803e540eaa9804e158f59
ducksquatarmy <unconfigured@null.spigotmc.org>
2023-05-31 15:58:24 -0700

    CHANGES

--- game/game.js
+++ game/game.js
@@ -2,6 +2,7 @@ let buttonElement = document.getElementById("btn");
 let stardustElement = document.getElementById("stardust");
 let stardust = 0;
 window.onload = () => {
+	SnackBar({ message: "Text" });
 	buttonElement.addEventListener("click", click);
 	if (localStorage.stardust) {
 		updateDOM();
@@ -23,4 +24,37 @@ function click() {
 }
 function updateDOM() {
 	stardustElement.innerHTML = "◈&#8201;" + localStorage.stardust;
+}
+
+function reset() {
+	swal({
+		title: "Warning!",
+		text:
+			"Are you sure you want to reset the game? All your progress will be lost!",
+		icon: "info",
+		buttons: ["Cancel", "Reset"],
+		dangerMode: true,
+	}).then((willDelete) => {
+		if (willDelete) {
+			swal({
+				title: "LAST WARNING!",
+				text: "FINAL WARNING: You will lose all your progress!",
+				icon: "warning",
+				buttons: ["EXIT", "RESET"],
+				dangerMode: true,
+			}).then((willDelete2) => {
+				if (willDelete2) {
+					localStorage.clear();
+					swal("Game Reset", "", {
+						icon: "error",
+						button: "Refresh",
+					}).then(() => {
+						location.reload();
+					});
+				} else {
+					swal("That was close!");
+				}
+			});
+		}
+	});
 }
